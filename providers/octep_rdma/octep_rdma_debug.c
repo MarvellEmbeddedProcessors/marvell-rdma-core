db6b02ff5ffb3abe30af5ec7ef9dcf995a638eb2
Kommula Shiva Shankar <kshankar@marvell.com>
2026-03-12 15:56:14 +0530

    prov/octep_rdma: add hex dump debug function
    add helper hex dump debug function
    
    Implementation derived from DPDK project
    
    Signed-off-by: Kommula Shiva Shankar <kshankar@marvell.com>

---
+++ providers/octep_rdma/octep_rdma_debug.c
@@ -0,0 +1,48 @@
+/* SPDX-License-Identifier: BSD-3-Clause
+ * Copyright(c) 2010-2014 Intel Corporation
+ */
+
+/* SPDX-License-Identifier: Marvell-MIT
+ * Copyright (c) 2025 Marvell.
+ */
+
+#include "octep_rdma_debug.h"
+
+#define LINE_LEN 128
+
+/* Function reused from DPDK eal_common_hexdump.c */
+void
+octep_rdma_hexdump(FILE *f, const char *title, const void *buf, unsigned int len)
+{
+	unsigned int i, out, ofs;
+	const unsigned char *data = buf;
+	char line[LINE_LEN]; /* space needed 8+16*3+3+16 == 75 */
+
+	fprintf(f, "%s at [%p], len=%u\n", title != NULL ? title : "  Dump data", data, len);
+	ofs = 0;
+	while (ofs < len) {
+		/* format the line in the buffer */
+		out = snprintf(line, LINE_LEN, "%08X:", ofs);
+		for (i = 0; i < 16; i++) {
+			if (ofs + i < len)
+				snprintf(line + out, LINE_LEN - out, " %02X",
+					 (data[ofs + i] & 0xff));
+			else
+				strcpy(line + out, "   ");
+			out += 3;
+		}
+
+		for (; i <= 16; i++)
+			out += snprintf(line + out, LINE_LEN - out, " | ");
+
+		for (i = 0; ofs < len && i < 16; i++, ofs++) {
+			unsigned char c = data[ofs];
+
+			if (c < ' ' || c > '~')
+				c = '.';
+			out += snprintf(line + out, LINE_LEN - out, "%c", c);
+		}
+		fprintf(f, "%s\n", line);
+	}
+	fflush(f);
+}
---
+++ providers/octep_rdma/octep_rdma_debug.h
@@ -0,0 +1,8 @@
+/* SPDX-License-Identifier: Marvell-MIT
+ * Copyright (c) 2025 Marvell.
+ */
+
+#include <stdio.h>
+#include <string.h>
+
+void octep_rdma_hexdump(FILE *f, const char *title, const void *buf, unsigned int len);
